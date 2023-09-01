892e918e31c1b1e86f8f329dde74c43a7e668d42
hmiftcahul11 <hmiftcahul11@gmail.com>
2023-09-02 02:05:49 +0700

    Update

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-// All Images
+// Slide Manual
+// const slides = document. querySelectorAll(".slide");
+// var counter = 0;
+// slides. forEach((slide, index) => {
+// slide.style.left = `${index * 100}%`;
+// });
+// const goPrev = () => {
+// if (counter == 0) {
+// counter = slides. length - 1;
+// slideImage;
+// } else {
+// counter--;
+// slideImage();
+// }}
+// const goNext = () => {
+// if (counter == slides.length - 1) {
+// counter = 0;
+// slideImage ();
+// } else {
+// counter++;
+// slideImage ();
+// }
+// };
+// const slideImage = () => {
+// slides.forEach((slide) => {
+// slide.style.transform =
+// `translate(-${counter * 100}%)`
+// });
+// };
+
+// Update => Slide Auto
 const slides = document.querySelectorAll(".slide");
 var counter = 0;
 slides.forEach((slide, index) => {
