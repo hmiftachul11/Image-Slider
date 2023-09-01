e4decf06e2d7b61af76a4eb4b388879705622fa3
hmiftcahul11 <hmiftcahul11@gmail.com>
2023-09-02 02:02:15 +0700

    Add Autoplay Slider

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-// / All Images
-const slides = document. querySelectorAll(".slide");
+// All Images
+const slides = document.querySelectorAll(".slide");
 var counter = 0;
-slides. forEach((slide, index) => {
-slide.style.left = `${index * 100}%`;
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
 });
+
 const goPrev = () => {
-if (counter == 0) {
-counter = slides. length - 1;
-slideImage;
-} else {
-counter--;
-slideImage();
-}}
+  if (counter == 0) {
+    counter = slides.length - 1;
+    slideImage();
+  } else {
+    counter--;
+    slideImage();
+  }
+};
+
 const goNext = () => {
-if (counter == slides.length - 1) {
-counter = 0;
-slideImage ();
-} else {
-counter++;
-slideImage ();
-}
+  if (counter == slides.length - 1) {
+    counter = 0;
+    slideImage();
+  } else {
+    counter++;
+    slideImage();
+  }
 };
+
 const slideImage = () => {
-slides.forEach((slide) => {
-slide.style.transform =
-`translate(-${counter * 100}%)`
-});
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
 };
+
+// Autoplay Slider (optional)
+let autoplayInterval;
+
+const startAutoplay = () => {
+  autoplayInterval = setInterval(goNext, 3000); // Ganti dengan interval yang Anda inginkan (dalam milidetik)
+};
+
+const stopAutoplay = () => {
+  clearInterval(autoplayInterval);
+};
+
+startAutoplay();
+
+// Pause autoplay on hover (optional)
+const container = document.querySelector(".container");
+container.addEventListener("mouseenter", stopAutoplay);
+container.addEventListener("mouseleave", startAutoplay);
