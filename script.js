9c93408577f9cac98551a87d63e8aaf29e56e100
hmiftcahul11 <hmiftcahul11@gmail.com>
2023-09-02 01:48:32 +0700

    Image Slider Javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+// / All Images
+const slides = document. querySelectorAll(".slide");
+var counter = 0;
+slides. forEach((slide, index) => {
+slide.style.left = `${index * 100}%`;
+});
+const goPrev = () => {
+if (counter == 0) {
+counter = slides. length - 1;
+slideImage;
+} else {
+counter--;
+slideImage();
+}}
+const goNext = () => {
+if (counter == slides.length - 1) {
+counter = 0;
+slideImage ();
+} else {
+counter++;
+slideImage ();
+}
+};
+const slideImage = () => {
+slides.forEach((slide) => {
+slide.style.transform =
+`translate(-${counter * 100}%)`
+});
+};
